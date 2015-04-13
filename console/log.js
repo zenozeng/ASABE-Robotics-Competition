565d1db3a60f8d970b822d87e9db285ae8b6e089
Zeno Zeng <zenoofzeng@gmail.com>
2015-04-13 12:33:09 +0800

    add console/

---
+++ console/log.js
@@ -0,0 +1,10 @@
+var log = [
+    {
+        timestamp: "Mon Apr 13 2015 12:29:48 GMT+0800 (CST)",
+        position: {
+            x: 0,
+            y: 2
+        },
+        message: "Position update: (0, 2)"
+    }
+];
