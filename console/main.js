3f083e594cfa72a9ac6115d3348762905315d9f3
Zeno Zeng <zenoofzeng@gmail.com>
2015-05-13 10:21:30 +0800

    draw every tree detected

--- console/main.js
+++ console/main.js
@@ -33,20 +33,30 @@ var writeLog = function(log) {
 var types = [];
 var treeCount = 0;
 var drawLog = function(log) {
-    // update tree count
-    if (log.tree) {
-        treeCount++;
+    if (!log.tree) {
+        return;
     }
+
+    // update tree count
+    treeCount++;
     var progress = parseInt(treeCount / 25 * 100) + '%';
     $('#progress').html('<h2>Current Progress</h2>' + treeCount + ' / 25 = ' + progress);
 
+    // update canvas
+    var left = 74;
+    var top = 280;
+    left += (log.tree.x - 1) * 39;
+    top -= (log.tree.y - 1) * 68;
+    var size = log.tree.height === "high" ? 36 : 18;
+    left -= size / 2;
+    top -= size / 2;
+    $('#canvas').append('<i class="fa fa-tree ' + log.tree.height+ ' ' + log.tree.color + '" style="top:' + top + 'px; left: ' + left + 'px"></i>');
+
     // update collected types
-    if (log.tree) {
-        var typeid = log.tree.height + log.tree.color;
-        if (types.indexOf(typeid) < 0) {
-            types.push(typeid);
-            $('#collected').append('<i class="fa fa-tree ' + log.tree.height+ ' ' + log.tree.color + '"></i>');
-        }
+    var typeid = log.tree.height + log.tree.color;
+    if (types.indexOf(typeid) < 0) {
+        types.push(typeid);
+        $('#collected').append('<i class="fa fa-tree ' + log.tree.height+ ' ' + log.tree.color + '"></i>');
     }
 };
 
