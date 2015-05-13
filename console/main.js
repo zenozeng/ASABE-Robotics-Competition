1f1af973c0853af8c42cc07000f33ee14754fc42
Zeno Zeng <zenoofzeng@gmail.com>
2015-05-13 09:58:50 +0800

    display current progress

--- console/main.js
+++ console/main.js
@@ -18,21 +18,38 @@ var bootTime = Date.now(); // fake boot time
  */
 var writeLog = function(log) {
     var d = new Date(bootTime + log.timestamp);
-    $('#logs').append('<p class="timestamp">' + d.toString() + '</p>');
-    $('#logs').append('<p class="message">' + log.message + '</p>');
+    if (log.tree) {
+        log.message = '<i class="fa fa-tree ' + log.tree.height+ ' ' + log.tree.color + '"></i>';
+        log.message += 'Tree detected: ' + JSON.stringify(log.tree);
+    }
+    $('#logs').prepend('<p class="message">' + log.message + '</p>');
+    $('#logs').prepend('<p class="timestamp">' + d.toString() + '</p>');
 };
 
 /**
  * Draw a log
  *
  */
+var treeCount = 0;
 var drawLog = function(log) {
+    if (log.tree) {
+        treeCount++;
+    }
+    var progress = parseInt(treeCount / 25 * 100) + '%';
+    $('#progress').html('<h2>Current Progress</h2>' + treeCount + ' / 25 = ' + progress);
 };
 
 /**
  * Main Logic
  */
 fetchLogs().then(function(logs) {
-    logs.forEach(writeLog);
-    logs.forEach(drawLog);
+    var process = function() {
+        var log = logs.shift();
+        writeLog(log);
+        drawLog(log);
+        if (logs.length > 0) {
+            setTimeout(process, 500);
+        }
+    };
+    process();
 });
