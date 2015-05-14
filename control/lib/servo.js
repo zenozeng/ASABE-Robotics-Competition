df9c4cc0ecf69e9784ca248013d9858f15b92984
Zeno Zeng <zenoofzeng@gmail.com>
2015-05-14 20:57:47 +0800

    add 约定, fixes #50

--- control/lib/servo.js
+++ control/lib/servo.js
@@ -1,6 +1,6 @@
 var pcduino = require('yapcduino');
 
-function setServoAngle(pin, angle) {
+function setAngle(pin, angle) {
     pcduino.pinMode(pin, pcduino.OUTPUT);
     var PWM_PERIOD = 20 * 1000;
     if (angle >= 0 && angle <= 180) {
@@ -10,9 +10,13 @@ function setServoAngle(pin, angle) {
         throw new Error('Invalid angle. angle should be >=0 && <=180');
     }
     var loops = 100; // 保持脉冲让机械臂有时间运动过去
-    pcduino.setPulse(pin, pulse, PWM_PERIOD, loops);
+    pcduino.digitalPulse(pin, pulse, PWM_PERIOD, loops);
+}
+
+// todo
+function lockAngle(pin, angle) {
 }
 
 module.exports = {
-    setServoAngle: setServoAngle
+    setAngle: setAngle
 };
--- control/lib/setpper-motor.js
+++ control/lib/setpper-motor.js
@@ -1,5 +1,11 @@
-var pcduino = require('pcduino');
+var pcduino = require('yapcduino');
 
-var digital = pcduino.digital;
+var PWM_PERIOD = 20 * 1000;
 
-console.log("There are " + digital.PIN_COUNT + " digital GPIO pins on the pcDuino.");
+var PIN_CLK = 0;
+var PIN_CW = 2; // 指定方向
+var PIN_EN = 3; // 决定是否在工作状态
+
+function forward(pin) {
+    pcduino.digitalPulse(pin, pulse, PWM_PERIOD, loops);
+}
