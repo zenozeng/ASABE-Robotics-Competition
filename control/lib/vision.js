c43d797c6463e2e9e786e373549a02ebf65c81bc
Zeno Zeng <zenoofzeng@gmail.com>
2015-05-14 23:16:25 +0800

    try opencv

--- control/lib/vision.js
+++ control/lib/vision.js
@@ -1,8 +1,19 @@
 // sudo apt-get install libgphoto2-2-dev
+// sudo apt-get install libcanberra-gtk0
 
-var gphoto2 = require('gphoto2');
-var GPhoto = new gphoto2.GPhoto2();
+// https://github.com/peterbraden/node-opencv/blob/05e42b70aa498cbae94713da1df31084090a2088/examples/coffeescript/camera.coffee
 
-GPhoto.list(function(list) {
-    console.log(list);
-});
+var cv = require('opencv');
+
+var camera = new cv.VideoCapture(0);
+
+var namedWindow = new cv.NamedWindow('Video', 0);
+
+setInterval(function() {
+    camera.read(function(err, im) {
+        if(im.width() > 0 && im.height() > 0) {
+            namedWindow.show(im);
+            namedWindow.blockingWaitKey(0, 100);
+        }
+    });
+}, 100);
