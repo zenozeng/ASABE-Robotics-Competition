29c5db212d9988558877ada50eae420fe92e67c6
Zeno Zeng <zenoofzeng@gmail.com>
2015-05-14 19:55:49 +0800

    update servo module

--- control/lib/servo.js
+++ control/lib/servo.js
@@ -1,6 +1,6 @@
 var pcduino = require('yapcduino');
 
-var setServoAngle = function(pin, angle) {
+function setServoAngle(pin, angle) {
     pcduino.pinMode(pin, pcduino.OUTPUT);
     var PWM_PERIOD = 20 * 1000;
     if (angle >= 0 && angle <= 180) {
@@ -9,6 +9,10 @@ var setServoAngle = function(pin, angle) {
     } else {
         throw new Error('Invalid angle. angle should be >=0 && <=180');
     }
-    var loops = 10; // 保持脉冲让机械臂有时间运动过去
+    var loops = 100; // 保持脉冲让机械臂有时间运动过去
     pcduino.setPulse(pin, pulse, PWM_PERIOD, loops);
+}
+
+module.exports = {
+    setServoAngle: setServoAngle
 };
