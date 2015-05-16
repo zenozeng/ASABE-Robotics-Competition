c596568bf476c5cbb53b377ef702e8d624166c76
Zeno Zeng <zenoofzeng@gmail.com>
2015-05-16 11:52:18 +0800

    add pin.js

---
+++ control/lib/pin.js
@@ -0,0 +1,9 @@
+// pin define
+
+// 0, 1, 2 口为舵机控制 pin
+
+module.exports = {
+    MANIPULATOR_SERVO: 0,
+    TANK_SERVO: 1,
+    END_EFFECTOR_SERVO: 2
+};
--- control/scp-yapc.sh
+++ control/scp-yapc.sh
@@ -1,1 +1,1 @@
-rsync -rv --exclude node_modules --exclude build ~/code/yapcduino/* ubuntu@10.75.44.25:control/node_modules/yapcduino
+rsync -rv --exclude node_modules --exclude build ~/code/yapcduino/* ubuntu@192.168.10.1:control/node_modules/yapcduino
