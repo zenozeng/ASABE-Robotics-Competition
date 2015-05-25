81f96ee4490da7fb527cd4f440c1d8135405552a
Zeno Zeng <zenoofzeng@gmail.com>
2015-05-25 21:25:02 +0800

    update 经验.md

--- control/scp-yapc.sh
+++ control/scp-yapc.sh
@@ -1,1 +1,1 @@
-rsync -rv --exclude node_modules --exclude build ~/code/yapcduino/* ubuntu@192.168.10.1:control/node_modules/yapcduino
+rsync -rv --exclude node_modules --exclude build ~/code/yapcduino/* ubuntu@ubuntu:control/node_modules/yapcduino
--- control/sync.sh
+++ control/sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -rv --exclude node_modules * ubuntu@192.168.10.1:control
+rsync -rv --exclude node_modules * ubuntu@ubuntu:control
