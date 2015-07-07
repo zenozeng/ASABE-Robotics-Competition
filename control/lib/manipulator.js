950c9495844e3bdbd5c1786d369d8f9081626926
Zeno Zeng <zenoofzeng@gmail.com>
2015-07-07 20:35:06 +0800

    update pins and manipulator

--- control/lib/manipulator.js
+++ control/lib/manipulator.js
@@ -1,2 +1,16 @@
-// 0 -- 默认最高位
-// 55
+var yapcduino = require('yapcduino');
+var Servo = yapcduino.Servo;
+var pins = require('./pins');
+
+var pin = pins.END_EFFECTOR_SERVO;
+
+pin = 5;
+
+var servo = new Servo(pin, {
+    minPWM: 1,
+    maxPWM: 200,
+    maxAngle: 180,
+    frequency: 390
+});
+
+// module.exports = servo;
--- control/lib/pins.js
+++ control/lib/pins.js
@@ -1,39 +1,46 @@
 module.exports = {
-    // 机械臂舵机
-    MANIPULATOR_SERVO: 0,
-
-    // Tank 底座旋转控制舵机
-    TANK_SERVO: 1,
-
-    // 夹持装置控制舵机
-    END_EFFECTOR_SERVO: 2,
-
-    // 左黑白循迹片
-    LEFT_BLACK_AND_WHITE_SENSOR: 3,
-
-    // 中循迹片
-    MIDDLE_BLACK_AND_WHITE_SENSOR: 4,
-
-    // 右循迹片
-    RIGHT_BLACK_AND_WHITE_SENSOR: 5,
-
-    // Tank 底座步进电机脉冲输入信号
-    TANK_STEPPING_MOTOR_CLK: 6,
-
-    // Tank 底座步进电机方向输入信号
-    TANK_STEPPING_MOTOR_CW: 7,
 
     // 左轮步进电机脉冲
-    LEFT_STEPPING_MOTOR_CLK: 8,
+    LEFT_STEPPING_MOTOR_CLK: 0,
 
     // 左轮步进电机方向
-    LEFT_STEPPING_MOTOR_CW: 9,
+    LEFT_STEPPING_MOTOR_CW: 1,
 
     // 右轮步进电机脉冲
-    RIGHT_STEPPING_MOTOR_CLK: 10,
+    RIGHT_STEPPING_MOTOR_CLK: 2,
 
     // 右轮步进电机方向
-    RIGHT_STEPPING_MOTOR_CW: 11
+    RIGHT_STEPPING_MOTOR_CW: 4,
+
+    // 机械臂步进电机脉冲
+    MANIPULATOR_STEPPING_MOTOR_CLK: 7,
+
+    // 机械臂步进电机方向
+    MANIPULATOR_STEPPING_MOTOR_CW: 8,
+
+    // 夹持装置控制舵机
+    END_EFFECTOR_SERVO: 3
+
+    // 左黑白循迹片
+    // LEFT_BLACK_AND_WHITE_SENSOR: 3,
+
+    // 中循迹片
+    // MIDDLE_BLACK_AND_WHITE_SENSOR: 4,
+
+    // 右循迹片
+    // RIGHT_BLACK_AND_WHITE_SENSOR: 5
+
+    // 机械臂舵机
+    // MANIPULATOR_SERVO: 0,
+
+    // Tank 底座旋转控制舵机
+    // TANK_SERVO: 1,
+
+    // Tank 底座步进电机脉冲输入信号
+    // TANK_STEPPING_MOTOR_CLK: 6,
+
+    // Tank 底座步进电机方向输入信号
+    // TANK_STEPPING_MOTOR_CW: 7,
 
     // 尾部舵机或者减速电机: 12
     // 左超声波 Trig: 13
