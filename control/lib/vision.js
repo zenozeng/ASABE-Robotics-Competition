1c053db6e561fb3f90aef95f8188fd1ed07bfd6b
Zeno Zeng <zenoofzeng@gmail.com>
2015-07-07 11:03:52 +0800

    send kill siginal to vision when lib/vision.js exit, fixes #100

--- control/lib/vision.js
+++ control/lib/vision.js
@@ -2,6 +2,11 @@ var path = require('path');
 var bin = path.join(__dirname, '../../vision/vision');
 var spawn = require('child_process').spawn;
 
+// Usage:
+// v = require('./vision');
+// will run on background
+// you can read this.exists || this.color || this.hue when you need it
+
 function Vision() {
     this.process = spawn(bin);
     this.process.stdout.on('data', function(data) {
@@ -13,8 +18,10 @@ function Vision() {
                 this.exists = obj.exists;
                 this.color = obj.color;
                 this.hue = obj.hue;
+                this.time = new Date();
             }
         } catch (e) {
+            console.warn('Ignore: ', e);
         }
     });
     this.process.stderr.on('data', function(data) {
@@ -23,6 +30,25 @@ function Vision() {
     return this;
 }
 
+Vision.prototype.log = function() {
+    var v = this;
+    console.log(JSON.stringify({
+        exists: v.exists,
+        color: v.color,
+        hue: v.hue,
+        time: v.time
+    }));
+};
+
 var vision = new Vision();
+
+process.on('exit', function(code) {
+    console.info('lib/vision.js: about to exit with code(' + code + '), try to kill vision bin process.');
+    if (vision.process) {
+        console.info('lib/vision.js: vision binary process detected - ', vision.process.pid);
+        vision.process.kill('SIGKILL');
+        console.info('lib/vision.js: process killed.');
+    }
+});
 
 module.exports = vision;
---
+++ control/test-vision.js
@@ -0,0 +1,1 @@
+var vision = require('./lib/vision');
