df7f1d844cb89b1ebbc8253f1704be4046e6fde8
Zeno Zeng <zenoofzeng@gmail.com>
2015-07-08 16:38:53 +0800

    use new car

--- control/lib/car.js
+++ control/lib/car.js
@@ -1,90 +1,107 @@
+require('yapcduino')({global: true});
 var pins = require('./pins');
-var pcduino = require('yapcduino');
 var whiteSensors = require('./white-sensors');
+
+var DEBUG = true;
+
+var log = function(message) {
+    if (DEBUG) {
+        console.log('lib/car.js: ' + message);
+    }
+};
 
 var left = {clk: pins.LEFT_STEPPING_MOTOR_CLK, cw: pins.LEFT_STEPPING_MOTOR_CW};
 var right = {clk: pins.RIGHT_STEPPING_MOTOR_CLK, cw: pins.RIGHT_STEPPING_MOTOR_CW};
 
+// init pinMode
 [left.clk, left.cw, right.clk, right.cw].forEach(function(pin) {
-    pcduino.pinMode(pin, pcduino.OUTPUT);
+    pinMode(pin, OUTPUT);
 });
 
-var leftMotor = new pcduino.SoftPWM(left.clk);
-var rightMotor = new pcduino.SoftPWM(right.clk);
+var leftMotor = new SoftPWM(left.clk);
+var rightMotor = new SoftPWM(right.clk);
 
-var period = 10000; // 或者 5000
+function Car() {
+    this.distance = 0;
+}
 
-var DEBUG = true;
-
-function stop() {
+Car.prototype.stop = function() {
     leftMotor.write(0);
     rightMotor.write(0);
-}
+};
 
-function forward() {
-    pcduino.digitalWrite(left.cw, pcduino.HIGH); // 左轮逆时针
-    pcduino.digitalWrite(right.cw, pcduino.LOW); // 右轮顺时针
+Car.prototype.go = function(leftCW, rightCW, leftSpeed, rightSpeed) {
+    // 设置方向
+    digitalWrite(left.cw, leftCW);
+    digitalWrite(right.cw, rightCW);
 
-    leftMotor.write(0.5, {period: period});
-    rightMotor.write(0.5, {period: period});
-}
+    // 设置速度
+    var period = 10000;
+    var motors = [leftMotor, rightMotor];
+    [leftSpeed, rightSpeed].forEach(function(speed, index) {
+        if (speed === 0) {
+            motors[index].write(0, {period: period});
+        } else {
+            motors[index].write(0.5, {period: parseInt(period / leftSpeed)});
+        }
+    });
+};
 
-function backward() {
-    pcduino.digitalWrite(left.cw, pcduino.LOW); // 左轮顺时针
-    pcduino.digitalWrite(right.cw, pcduino.HIGH); // 右轮逆时针
+Car.prototype.forward = function() {
+    // 左轮逆时针，右轮顺时针
+    log('forward');
+    this.go(HIGH, LOW, 1, 1);
+};
 
-    leftMotor.write(0.5, {period: period});
-    rightMotor.write(0.5, {period: period});
-}
+Car.prototype.backward = function() {
+    // 左轮顺时针，右轮逆时针
+    log('backward');
+    this.go(LOW, HIGH, 1, 1);
+};
 
-function turnLeft() {
-    pcduino.digitalWrite(left.cw, pcduino.HIGH, period);
-    pcduino.digitalWrite(right.cw, pcduino.LOW, period);
+Car.prototype.turnLeft = function() {
+    log('turn left');
+    this.go(HIGH, LOW, 0, 1);
+};
 
-    leftMotor.write(0, {period: period});
-    rightMotor.write(0.5, {period: period});
-}
+Car.prototype.turnRight = function() {
+    log('turn right');
+    this.go(HIGH, LOW, 1, 0);
+};
 
-function turnRight() {
-    pcduino.digitalWrite(left.cw, pcduino.HIGH, period);
-    pcduino.digitalWrite(right.cw, pcduino.LOW, period);
-
-    leftMotor.write(0.5, {period: period});
-    rightMotor.write(0, {period: period});
-}
-
-function auto() {
-    setInterval(function() {
+Car.prototype.autoForward = function() {
+    var car = this;
+    car.autoForwardInterval = setInterval(function() {
         var leftIsBlack = whiteSensors.left.isBlack();
         var middleIsBlack = whiteSensors.middle.isBlack();
         var rightIsBlack = whiteSensors.right.isBlack();
         if (DEBUG) {
             console.log([leftIsBlack, middleIsBlack, rightIsBlack]);
         }
+
         if (leftIsBlack && rightIsBlack) {
             // 转弯吧
             return;
         }
         if (leftIsBlack) {
-            turnLeft();
-            console.log('turn left');
+            car.turnLeft();
             return;
         }
         if (rightIsBlack) {
-            turnRight();
-            console.log('turn right');
+            car.turnRight();
             return;
         }
-        console.log('forward');
-        forward();
-    }, 100);
-}
+        car.forward();
 
-module.exports = {
-    auto: auto,
-    stop: stop,
-    forward: forward,
-    backward: backward,
-    turnLeft: turnLeft,
-    turnRight: turnRight
+    }, 100);
 };
+
+Car.prototype.stopAutoForward = function() {
+    if (typeof this.autoForwardInterval !== "undefined") {
+        clearInterval(this.autoForwardInterval);
+    }
+};
+
+var car = new Car();
+
+module.exports = car;
