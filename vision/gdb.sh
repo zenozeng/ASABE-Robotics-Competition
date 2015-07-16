6c4c2962243206644c7c8893bcac3786205a6266
Zeno Zeng <zenoofzeng@gmail.com>
2015-07-16 19:43:10 +0800

    k-means

---
+++ vision/gdb.sh
@@ -0,0 +1,2 @@
+g++ main.cpp -o vision -DDEBUG=true -g -lopencv_core -lopencv_imgproc -lopencv_highgui -lm
+gdb vision
--- vision/main.cpp
+++ vision/main.cpp
@@ -11,34 +11,6 @@ using namespace cv;
 // http://docs.opencv.org/modules/highgui/doc/reading_and_writing_images_and_video.html
 // 分离颜色通道&多通道图像混合：http://blog.csdn.net/poem_qianmo/article/details/21176257
 
-/**
- * Replacement for Matlab's bwareaopen()
- * Input image must be 8 bits, 1 channel, black and white (objects)
- * with values 0 and 255 respectively
- * From: http://opencv-code.com/quick-tips/code-replacement-for-matlabs-bwareaopen/
- */
-// void removeSmallBlobs(cv::Mat& im, double size)
-// {
-
-//     // Only accept CV_8UC1
-//     if (im.channels() != 1 || im.type() != CV_8U)
-//         return;
-
-//     // Find all contours
-//     std::vector<std::vector<cv::Point> > contours;
-//     cv::findContours(im.clone(), contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
-
-//     for (int i = 0; i < contours.size(); i++)
-//     {
-//         // Calculate contour area
-//         double area = cv::contourArea(contours[i]);
-
-//         // Remove small objects by drawing the contour with black color
-//         if (area > 0 && area <= size)
-//             cv::drawContours(im, contours, i, CV_RGB(0,0,0), -1);
-//     }
-// }
-
 int main()
 {
     cout << "Vision Started." << endl;
@@ -71,6 +43,12 @@ int main()
         Mat frame;
         capture >> frame;
 
+        ////////////////////////////////////////
+        //
+        // 准备工作
+        //
+        ////////////////////////////////////////
+
         // narrow to 1:1 square based on height
         int width = frame.cols;
         int height = frame.rows;
@@ -88,94 +66,140 @@ int main()
         l = hlsChannels.at(1);
         s = hlsChannels.at(2);
 
-        ////////////////////////////////////////
-        //
-        // 判断树是否存在
-        //
-        ////////////////////////////////////////
-
         Rect roi_rect(0, height * 0.75, width, height * 0.25); // x, y, width, height
         Mat ROI_S = s(roi_rect);
         Mat ROI_H = h(roi_rect);
         Mat ROI_L = l(roi_rect);
 
-        Mat ROI_BW;
-        // http://docs.opencv.org/doc/tutorials/imgproc/threshold/threshold.html
-        // 1: Binary Inverted
-        threshold(ROI_L, ROI_BW, 192, 255, 1);
+        /////////////////////////////////////////////////////////////////////////////////////////////////
+        //
+        // 判断树是否存在（亮度 K-Means, K=3）
+        //
+        // double kmeans(InputArray data, int K, InputOutputArray bestLabels, TermCriteria criteria,
+        //               int attempts, int flags, OutputArray centers=noArray() )
+        //
+        // http://docs.opencv.org/modules/core/doc/clustering.html
+        // http://seiya-kumada.blogspot.jp/2013/03/k-means-clustering.html
+        //
+        /////////////////////////////////////////////////////////////////////////////////////////////////
 
-        // removeSmallBlobs(ROI_BW, 1000);
+        height = ROI_L.rows;
+        width = ROI_L.cols;
+
+        Mat labels;
+        Mat centers;
+
+        Mat src = ROI_L;
+
+        src.convertTo(src, CV_32F);
+        kmeans(src, 2, labels,
+               TermCriteria(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS, 10, 0.001), 5,
+               KMEANS_PP_CENTERS, centers);
+
+        Mat new_image( src.size(), src.type() );
+
+        cout << src.size() << endl;
+
+        for( int y = 0; y < src.rows; y++ )
+        {
+            for( int x = 0; x < src.cols; x++ )
+            {
+                cout << "p3" << endl << flush;
+                cout << labels.size() << endl;
+                int cluster_idx = labels.at<int>(y + x * src.rows, 0);
+                cout << "<" << cluster_idx << ">" << endl << flush;
+                cout << "p4" << endl << flush;
+                cout << x << "," << y << endl << flush;
+                cout << centers.at<float>(cluster_idx, 0) << endl << flush;
+                new_image.at<Vec3b>(y,x)[0] = centers.at<float>(cluster_idx, 0);
+                cout << "p5" << endl << flush;
+                // new_image.at<Vec3b>(y,x)[1] = centers.at<float>(cluster_idx, 1);
+                // new_image.at<Vec3b>(y,x)[2] = centers.at<float>(cluster_idx, 2);
+            }
+        }
+
+        Mat ROI_BW = new_image;
 
         if (DEBUG) {
-            imshow("roi_l", ROI_L);
-            imshow("roi_bw", ROI_BW);
-        }
-
-
-        double rate = sum(ROI_BW)[0] / (ROI_BW.rows * ROI_BW.cols * 255);
-
-        if (DEBUG) {
-            cout << "rate: ";
-            cout << rate << endl;
-        }
-
-        bool exists = rate > 0.15;
-
-        // 若树存在，判断其颜色类型
-        string color = "null";
-        double hue;
-
-        if (exists) {
-            int count = 0;
-            int hueSum = 0;
-            for (int y = 0; y < ROI_BW.rows; y++) {
-                for (int x = 0; x < ROI_BW.cols; x++) {
-                    if (ROI_BW.at<Vec3b>(y, x)[0] == 255) {
-                        count++;
-                        hueSum += ROI_H.at<Vec3b>(y, x)[0];
-                    }
-                }
-            }
-            hue = 1.0 * hueSum / count / 255 * 360;
-
-            if (hue > 35 && hue < 70) {
-                color = "Yellow";
-            } else if (hue > 70 && hue < 240) {
-                color = "Green";
-            } else {
-                color = "Brown";
-            }
-        }
-
-        // 获取树像素的重心位置
-        double position = 0;
-        if (exists) {
-            int count = 0;
-            int count_t = 0;
-
-            for (int y = 0; y < ROI_BW.rows; y++) {
-                for (int x = 0; x < ROI_BW.cols; x++) {
-                    if (ROI_BW.at<Vec3b>(y, x)[0] == 255) {
-                        position += x;
-                        count++;
-                    }
-                }
-            }
-
-            position /= count;
-            position /= ROI_BW.cols;
-        }
-
-        cout << "{\"exists\": \"" << exists << "\", \"color\": \"" << color << "\", \"hue\": \"" << (int) hue << "\", \"position\": \"" << position << "\"}" << endl;
-
-        Scalar grey(255 * 0.1, 255 * 0.1, 255 * 0.1);
-
-        imwrite("/run/shm/frame.jpg", frame);
-
-        if (DEBUG) {
+            cout << "debug" << endl;
+            imshow("tmp", ROI_BW);
             imshow("frame", frame);
+            // imshow("roi_l", ROI_L);
             waitKey(100);
         }
+
+
+        // double rate = sum(ROI_BW)[0] / (ROI_BW.rows * ROI_BW.cols * 255);
+
+        // if (DEBUG) {
+        //     cout << "rate: ";
+        //     cout << rate << endl;
+        // }
+
+        // bool exists = rate > 0.15;
+
+        // // 若树存在，判断其颜色类型
+        // string color = "null";
+        // double hue;
+
+        // if (exists) {
+        //     int count = 0;
+        //     int hueSum = 0;
+        //     for (int y = 0; y < ROI_BW.rows; y++) {
+        //         for (int x = 0; x < ROI_BW.cols; x++) {
+        //             if (ROI_BW.at<Vec3b>(y, x)[0] == 255) {
+        //                 count++;
+        //                 hueSum += ROI_H.at<Vec3b>(y, x)[0];
+        //             }
+        //         }
+        //     }
+        //     hue = 1.0 * hueSum / count / 255 * 360;
+
+        //     if (hue > 35 && hue < 70) {
+        //         color = "Yellow";
+        //     } else if (hue > 70 && hue < 240) {
+        //         color = "Green";
+        //     } else {
+        //         color = "Brown";
+        //     }
+        // }
+
+        // // 获取树像素的重心位置
+        // double position = 0;
+        // if (exists) {
+        //     int count = 0;
+        //     int count_t = 0;
+
+        //     for (int y = 0; y < ROI_BW.rows; y++) {
+        //         for (int x = 0; x < ROI_BW.cols; x++) {
+        //             if (ROI_BW.at<Vec3b>(y, x)[0] == 255) {
+        //                 position += x;
+        //                 count++;
+        //             }
+        //         }
+        //     }
+
+        //     position /= count;
+        //     position /= ROI_BW.cols;
+        // }
+
+        // // output json
+        // cout << "{";
+        // cout << "\"exists\": \"" << exists << "\", ";
+        // cout << "\"color\": \"" << color << "\", ";
+        // cout << "\"hue\": \"" << (int) hue << "\", ";
+        // cout << "\"position\": \"" << position << "\"";
+        // cout << "}" << endl;
+        // cout << flush;
+
+        // Scalar grey(255 * 0.1, 255 * 0.1, 255 * 0.1);
+
+        // imwrite("/run/shm/frame.jpg", frame);
+
+        // if (DEBUG) {
+        //     imshow("frame", frame);
+        //     waitKey(100);
+        // }
     }
 
     return 0;
