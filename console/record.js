5647b30321e8ad3ef6cb8f82a1f90f3d66c7c9b1
Zeno Zeng <zenoofzeng@gmail.com>
2015-07-18 23:02:24 +0800

    record mode

---
+++ console/record.js
@@ -0,0 +1,20 @@
+$(function() {
+    window.recordMode = false;
+    var interval;
+    var count = 0;
+    $('#record-mode').click(function() {
+        window.recordMode = !window.recordMode;
+        $('#record-mode').toggleClass('open');
+        if (window.recordMode) {
+            interval = setInterval(function() {
+                count++;
+                var $a = $('<a></a>').attr('href', "frame.jpg?_=" + Date.now());
+                $a.attr('download', count);
+                $('body').append($a);
+                $a[0].click();
+            }, 500);
+        } else {
+            clearInterval(interval);
+        }
+    });
+});
